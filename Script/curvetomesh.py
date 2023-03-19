2c880f7a87b331a97d3fb423348760d969784f0c
koiusa <koiusa@users.noreply.github.com>
2023-03-19 18:50:02 +0900

    カーブをメッシュに変換するスクリプトを追加

--- Script/curvetomesh.py
+++ Script/curvetomesh.py
@@ -1,9 +1,8 @@
 import bpy
 
 
-def apply_modifier(arg_object: bpy.types.Object) -> bool:
-    """指定オブジェクトのモディファイアを適用する
-       ※アーマチュア以外
+def curve_to_mesh(arg_object: bpy.types.Object) -> bool:
+    """カーブをメッシュに変換
 
     Keyword Arguments:
         arg_object (bpy.types.Object): 指定オブジェクト
@@ -11,14 +10,13 @@ def apply_modifier(arg_object: bpy.types.Object) -> bool:
     Returns:
         bool -- 実行成否
     """
-
     # 対象のオブジェクトをアクティブオブジェクトにする
     bpy.context.view_layer.objects.active = arg_object
 
-    # オブジェクト内の全てのモディファイアを走査する
-    for modifier in arg_object.modifiers:
-        if modifier.type != 'ARMATURE':
-            bpy.ops.object.modifier_apply(modifier=modifier.name)
+    # カーブをメッシュに変換
+    if arg_object.type == 'CURVE':
+        arg_object.select_set(True)
+        bpy.ops.object.convert(target='MESH')
 
     return True
 
@@ -29,4 +27,4 @@ bpy.ops.object.select_all(action='DESELECT')
 # 関数の実行
 objects = bpy.context.scene.objects
 for obj in objects:
-    apply_modifier(obj)
+    curve_to_mesh(obj)
